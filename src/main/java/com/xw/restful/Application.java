721e2efb44996ba1dc3cbd2b9ccdbc389e051661
gaoqiong <gaoqiong.xie@uebuycar.com>
2019-01-23 13:52:31 +0800

    打包

--- src/main/java/com/xw/restful/Application.java
+++ src/main/java/com/xw/restful/Application.java
@@ -3,6 +3,8 @@ package com.xw.restful;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * 应用启动类
@@ -12,10 +14,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 @SpringBootApplication
 @MapperScan(basePackages = {"com.xw.restful.dao"})
-public class Application {
-//	private static Logger logger = Logger.getLogger(Application.class);
+public class Application extends SpringBootServletInitializer{
 	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
+	@Override//为了打包springboot项目
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(this.getClass());
+    }
 }
