47adf5f7321ba8c543139bcbf8ac76fff3e4140c
gaoqiong <gaoqiong.xie@uebuycar.com>
2019-02-28 13:32:14 +0800

    登录跳转

--- src/main/webapp/scripts/common.js
+++ src/main/webapp/scripts/common.js
@@ -28,7 +28,7 @@ $(function(){
 		        //如果数据请求验证时，对应的请求资源(路径)没有权限(或者没有登录)
 		        if (reqObj && reqObj.errorCode) {
 		        	var loginErrorCodeArr = ['0004','0005','0006','0007'];
-		        	if(loginErrorCodeArr.toString().indexOf(reqObj.errorCode)>0){
+		        	if(loginErrorCodeArr.toString().indexOf(reqObj.errorCode)>-1){
 		        		$.messager.alert('提示',reqObj.msg,'warning');
 		        		return window.location.href = "../login.html";
 	        		}
