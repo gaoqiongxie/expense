06c9403f92c3a2c11dc7c06a15d726956869f8ea
gaoqiong <gaoqiong.xie@uebuycar.com>
2019-03-08 13:40:04 +0800

    add test

--- src/test/java/com/xw/test/expense/AuthTest.java
+++ src/test/java/com/xw/test/expense/AuthTest.java
@@ -1,11 +1,17 @@
 package com.xw.test.expense;
 
+import java.lang.reflect.InvocationTargetException;
+
+import org.apache.commons.beanutils.BeanUtils;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.xw.restful.dao.FmlAuthDao;
 import com.xw.restful.domain.FmlAuth;
+import com.xw.restful.domain.auth.TokenModel;
+import com.xw.restful.domain.auth.UserAuth;
 import com.xw.restful.service.AuthService;
+import com.xw.restful.utils.BaseUtils;
 import com.xw.restful.utils.rsa.MD5;
 
 public class AuthTest extends ExpenseApplicationTests{
@@ -44,5 +50,82 @@ public class AuthTest extends ExpenseApplicationTests{
 	@Test
 	public void refreshTest() {
 		o = authService.refresh("");
+	}
+	
+	@Test
+	public void authTest() {
+		//login
+		TokenModel tokenModel_login = login("gg", "3030");
+		if(null == tokenModel_login) {
+			printf("login fail");
+			return;
+		}
+		printf("login success");
+		printfJSON(tokenModel_login);
+		
+		//validate
+		printf(validate(tokenModel_login));
+		
+		printf("睡眠 30 分钟");
+		sleepMinute(30);
+		
+		//refresh
+		String refreshToken = tokenModel_login.getUserAuth().getRefreshToken();
+		UserAuth userAuth = (UserAuth) authService.refresh(refreshToken);
+		if(null == userAuth) {
+			printf("refreshToken fail");
+			return;
+		}
+		TokenModel tokenModel_refresh = new TokenModel();
+		try {
+			BeanUtils.copyProperties(tokenModel_login, tokenModel_refresh);
+			tokenModel_refresh.setUserAuth(userAuth);
+			
+		} catch (IllegalAccessException e) {
+			e.printStackTrace();
+		} catch (InvocationTargetException e) {
+			e.printStackTrace();
+		}
+		printf("refresh success");
+		printfJSON(tokenModel_refresh);
+		
+		
+		//validate
+		printf(validate(tokenModel_refresh));
+		
+		//logout
+		
+		
+		
+	}
+	
+	private TokenModel login(String userCode, String userPassword) {
+		map.put("userCode", "gg");
+		map.put("userPassword", "3030");
+		TokenModel tokenModel = (TokenModel) authService.login(apiRequest);
+		return tokenModel;
+	}
+
+	private boolean validate(TokenModel tokenModel) {
+		String acceessToken = tokenModel.getUserAuth().getAccessToken();
+		return authService.validateAccessToken(acceessToken);
+	}
+
+	private void printf(Object o) {
+		System.out.println(o);
+	}
+	
+	private void printfJSON(Object o) {
+		System.out.println(BaseUtils.toJSONString(o));
+	}
+	
+	private void sleepMinute(long minute) {
+		try {
+			new Thread();
+			printf("睡眠 "+minute+" 分钟");
+			Thread.sleep(minute*60*1000l);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 	}
 }
